e9c2e51f0aa16ecec813908b3d1ead20dc52b509
Joe Hitchens <joe@sleepless.com>
2012-12-20 01:31:33 -0800

    -

--- README.md
+++ README.md
@@ -3,13 +3,101 @@
 
 ## Install
 
-npm install dadu
+	npm install dadu
 
 ## Example
 
-## API
+### Server
+
+	dadu = require("dadu");
+	require('http').createServer(function(req, res) {
+
+		dadu.handleUpload(req, res);
+
+	}).listen(4080);
+
+
+There are a few options you can pass in with an object.
+
+		var options = {
+
+			// where you want uploaded files to land.  Must already exist
+			// default is "/tmp"
+			fsPath: "/tmp",
+		
+			// a regular expression used to sanitize filenames before writing
+			// default is /[^-._a-z0-9]+/g;
+			reClean = /[^-._a-z0-9]+/g;
+
+		}
+
+		dadu.handleUpload(req, res, options);
+
+And lastly, if you include a callback, this will be called with the full path of
+the uploaded file for you.
+
+		dadu.handleUpload(req, res, {
+			cb: function(error, path) {
+				if(error)
+					// something went wrong
+				// path == the filesystem path where uploaded file was stored
+			}
+		});
+
+
+### Browser
+
+The browser and server both use the exact same dadu.js file.
+The code deduces whether it's running in a node process or not and acts accordingly.
+
+	<html>
+	<body>
+		<div id=drop>Drop a file on me.</div>
+		<script src='dadu.js'></script>
+		<script>
+			var drop = document.getElementById('drop')
+			dadu = new Dadu();
+			dadu.target(drop);
+		</script>
+	</body>
+	</html>
+
+
+The target() function also can take an options object.
+
+	dadu.target(drop, {
+		sent: function(xfer) {
+			// called when upload is complete 
+			// arg is the xfer that just completed
+			alert('file uploaded ok: ' + o2j(o));
+		},
+		status: function(xfers) {
+			// called every 1/4 second or so with array of xfer objects
+		},
+		enter: function(event) {
+			// called when mouse enters the target element.  
+			// so you can do some sort of highlight effect, for example
+		},
+		leave: function(event) {
+			// called when mouse leaves the target element.  
+		},
+	})
+
+
+## Test
+
+
+To test, run the server ...
+
+	node dadu.js
+
+Then load localhost:4080 in your browser.
+When the server is in testmode it will return a test page that you can
+drop files onto.
+
 
 ## License
+
 
 	Copyright 2011 Sleepless Software Inc. All rights reserved.
 
--- dadu.js
+++ dadu.js
@@ -20,168 +20,483 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE. 
 */
+	
+var j2o = function(j) { try { return JSON.parse(j) } catch(e) { return null } }
+var o2j = function(o) { return JSON.stringify(o) }
 
 
-var dadu = {
+if((typeof process) !== 'undefined') {
 
-	xfers: { files: [] },
+	// ========================
+    // node.js
+	// ========================
 
-	target: function(target, cbStatus, cbEnter, cbLeave, cbSent, url) {
-		var xfers = dadu.xfers
-		var nop = function() {}
+	exports.handleUpload = function(req, res, opts) {
 
-		if(typeof target === "string")
-			target = document.getElementById(target)
-		if(typeof target !== "object")
-			target = document.body
+		opts = opts || {};
+		var fsPath = opts.fsPath || "/tmp";
+		var reClean = opts.reClean || /[^-._a-z0-9]+/g;
 
-		target.ondragenter = function(event) {
-			event.preventDefault();
-			if(cbEnter)
-				cbEnter(event)
-			return true;
+		var cb = function(err, o) {
+			o = o || {};
+				o.error = err;
+			var j = o2j(o);
+			res.writeHead(200, {
+				//"Access-Control-Allow-Origin": "*",
+				"Access-Control-Max-Age": "0",
+				"Content-Type": "application/json",
+				"Content-Length": j.length
+			})
+			res.end(j)
+
+			// XXX callback to 
+			if(opts.cb) {
+				opts.cb(err, path);
+			}
 		}
 
-		target.ondragleave = cbLeave || nop
+		var u = require("url").parse(req.url, true)
+		var query = u.query
 
-		target.ondragover = function(event) {
-			event.preventDefault();
-			return true;
-		}
+		var file = query.file || "";
+		file = file.toLowerCase().replace(reClean, "_")
+		if(!file)
+			return cb("No file");
+		if(file.match("/\.\./"))
+			return cb("Naughty file name: "+file);
 
-		target.ondrop = function(event) {
-			event.preventDefault();
+		var path = fsPath + "/" + file;
 
-			(cbLeave || nop)(event)
 
-			var newFiles = event.dataTransfer.files
-			var l = newFiles.length
-			var ticking = true
-			var i, file, tick
+		var ws = require("fs").createWriteStream(path)
 
-			if(xfers.files.length < 1 && !xfers.current) {
-				// nothing in queue or in transit; clear counts and arrays
-				ticking = false
-				xfers.ok = []
-				xfers.error = []
-				xfers.current = null
-				xfers.filesTotal = 0
-				xfers.filesDone = 0
-				xfers.filesFaild = 0
-				xfers.total = 0
-				xfers.sofar = 0
-				xfers.percent = 0
-				xfers.done = false
+		req.soFar = 0;
+
+		req.addListener("data", function(d) {
+			req.soFar += d.length;
+			ws.write(d, "binary")
+		})
+		
+		req.addListener("end", function() {
+			ws.end() 
+			cb(null, { file: file, size: req.soFar });
+		})
+
+	}
+
+
+	if(require.main === module) {
+		// ====================
+		// test mode
+		// ====================
+		require('http').createServer(function(req, res) {
+
+			console.log(req.method + " " + req.url);
+			if(req.url == "/") {
+				var data = ""+
+"<html>"+"\n"+
+"<body>"+"\n"+
+"<div id=drop>Drop a file on me.</div>"+"\n"+
+"<script src='dadu.js'></script>"+"\n"+
+"<script>"+"\n"+
+"	dadu = new Dadu();"+"\n"+
+"	dadu.target('drop', {"+"\n"+
+"		sent: function(o) { alert('file uploaded ok: ' + o2j(o)); },"+"\n"+
+"	})"+"\n"+
+"</script>"+"\n"+
+"";
+				res.writeHead(200, {
+					"Content-Type": "text/html",
+					"Content-Length": data.length
+				})
+				res.end(data);
+				return;
 			}
 
-			// add new files to queue.  there may already be transfers in progress
-			for(i = 0; i < l; i++) {
-				file = newFiles[i]
-				xfers.files.push(file)
-				xfers.total += file.size
-				xfers.filesTotal++
-				if(file.fileName === undefined) {
-					// latest FF
-					file.fileName = file.name;
-				}
+			if(req.url == "/dadu.js") {
+				require("fs").readFile("dadu.js", function(err, data) {
+					res.writeHead(200, {
+						"Content-Type": "application/javascript",
+						"Content-Length": data.length
+					})
+					res.end(data);
+				})
+				return;
 			}
+		
+			exports.handleUpload(req, res);
 
-			if(!ticking)
-				dadu.tick(cbStatus, cbSent, url)
-		}
-	},
-
-	tick: function(cbStatus, cbSent, url) {
-		var loc = document.location
-		var xfers = dadu.xfers
-		var l = xfers.files.length
-		var r, file, i , url
-
-		if(!xfers.current) {
-			// nothing currently being sent
-			if(l < 1) {
-				// queue drained. make one last status callback with done=true
-				xfers.done = true;
-				xfers.sofar = xfers.total
-				xfers.percent = 100
-				if(cbStatus)
-					cbStatus(xfers)
-				return	// return, don't restart timer
-			}
-
-			// get next file from queue
-			file = xfers.files.shift()
-
-			// make this the current transfer in progress
-			xfers.current = file
-
-			// start sending it
-			if(typeof ActiveXObject != "undefined") 
-				r = new ActiveXObject("Microsoft.XMLHTTP");
-			else
-				r = new XMLHttpRequest();
-			r.upload.addEventListener("progress", function(e) {
-				if(e.lengthComputable)
-					file.loaded = e.loaded 
-			}, false)
-			r.onload = function() {
-				var hashName = r.responseText
-
-				file.ok = true
-				file.hashName = JSON.parse(r.responseText).hash
-				xfers.ok.push(file)
-				xfers.current = null
-				xfers.filesDone++
-				if(cbSent)
-					cbSent(file)
-			}
-			r.upload.addEventListener("error", function(e) {
-				alert('error');
-				file.error = e
-				xfers.error.push(file)
-				xfers.filesFailed++
-				xfers.current = null
-				xfers.filesDone++
-			}, false)
-			r.upload.addEventListener("abort", function(e) {
-				alert('abort');
-				file.aborted = e
-				xfers.error.push(file)
-				xfers.filesFailed++
-				xfers.current = null
-				xfers.filesDone++
-			}, false)
-			//if(!url) {
-				url = loc.protocol + "//" + loc.hostname + ":4080"
-			//}
-			url += "/?file="+encodeURIComponent(file.fileName)
-			r.open("POST", url, true);
-			r.setRequestHeader("Content-Type", "text/plain") // required for chrome - go figure
-			r.send(file);
-		}
-
-
-		// compute overall progress
-		xfers.sofar = 0
-		for(i = 0; i < xfers.ok.length; i++) {
-			xfers.sofar += xfers.ok[i].size
-		}
-		for(i = 0; i < xfers.error.length; i++) {
-			xfers.sofar += xfers.error[i].size
-		}
-		if(xfers.current) {
-			xfers.sofar += xfers.current.loaded || 0
-		}
-		xfers.percent = Math.floor((xfers.sofar * 100) / xfers.total)
-
-
-		// call back with current status
-		if(cbStatus)
-			cbStatus(xfers)
-
-		setTimeout(dadu.tick, 250, cbStatus, cbSent, url)
+		}).listen(4080);
+		console.log("listening");
 	}
 
 }
+else {
+
+	// ========================
+    // browser
+	// ========================
+
+	var Dadu = function(opts) {
+
+		var self = this;
+
+		opts = opts || {};
+
+		var port = opts.port || 4080;
+
+		var xfers = { files: [] }
+
+		self.target = function(target, cbStatus, cbEnter, cbLeave, cbSent) {
+
+			//var xfers = self.xfers
+			var nop = function() {}
 
 
+			if(typeof cbStatus === "object") {
+				var o = cbStatus;
+				cbStatus = o.status || nop();
+				cbEnter = o.enter || nop();
+				cbLeave = o.leave || nop();
+				cbSent = o.sent || nop();
+			}
+
+
+			if(typeof target === "string")
+				target = document.getElementById(target)
+			if(typeof target !== "object")
+				target = document.body
+
+			target.ondragenter = function(event) {
+				event.preventDefault();
+				if(cbEnter)
+					cbEnter(event)
+				return true;
+			}
+
+			target.ondragleave = cbLeave || nop
+
+			target.ondragover = function(event) {
+				event.preventDefault();
+				return true;
+			}
+
+			target.ondrop = function(event) {
+				event.preventDefault();
+
+				(cbLeave || nop)(event)
+
+				var newFiles = event.dataTransfer.files
+				var l = newFiles.length
+				var ticking = true
+				var i, file, tick
+
+				if(xfers.files.length < 1 && !xfers.current) {
+					// nothing in queue or in transit; clear counts and arrays
+					ticking = false
+					xfers.ok = []
+					xfers.error = []
+					xfers.current = null
+					xfers.filesTotal = 0
+					xfers.filesDone = 0
+					xfers.filesFaild = 0
+					xfers.total = 0
+					xfers.soFar = 0
+					xfers.percent = 0
+					xfers.done = false
+				}
+
+				// add new files to queue.  there may already be transfers in progress
+				for(i = 0; i < l; i++) {
+					file = newFiles[i]
+					xfers.files.push(file)
+					xfers.total += file.size
+					xfers.filesTotal++
+					if(file.fileName === undefined) {
+						// latest FF
+						file.fileName = file.name;
+					}
+				}
+
+				if(!ticking)
+					self.tick(cbStatus, cbSent)
+			}
+		}
+
+		self.tick = function(cbStatus, cbSent) {
+			//var xfers = self.xfers
+			var l = xfers.files.length
+			var r, file, i
+
+			if(!xfers.current) {
+				// nothing currently being sent
+
+				if(l < 1) {
+					// queue drained. make one last status callback with done=true
+					xfers.done = true;
+					xfers.soFar = xfers.total
+					xfers.percent = 100
+					if(cbStatus)
+						cbStatus(xfers)
+					return	// don't restart timer
+				}
+
+				// get next file from queue
+				file = xfers.files.shift()
+
+				// make this the current transfer in progress
+				xfers.current = file
+
+				// start sending it
+				if(typeof ActiveXObject != "undefined") 
+					r = new ActiveXObject("Microsoft.XMLHTTP");
+				else
+					r = new XMLHttpRequest();
+
+				r.upload.addEventListener("progress", function(e) {
+					if(e.lengthComputable)
+						file.loaded = e.loaded 
+				}, false)
+
+				r.onload = function() {
+					var o = j2o(r.responseText) || {error: "Upload failed"};
+
+					file.ok = true
+					file.remoteName = o.file;
+					file.remoteSize = o.size;
+					xfers.ok.push(file)
+					xfers.current = null
+					xfers.filesDone++
+					if(cbSent)
+						cbSent(file)
+				}
+
+				r.upload.addEventListener("error", function(e) {
+					alert('Upload error.');
+					file.error = e
+					xfers.error.push(file)
+					xfers.filesFailed++
+					xfers.current = null
+					xfers.filesDone++
+				}, false)
+
+				r.upload.addEventListener("abort", function(e) {
+					alert('Upload aborted.');
+					file.aborted = e
+					xfers.error.push(file)
+					xfers.filesFailed++
+					xfers.current = null
+					xfers.filesDone++
+				}, false)
+
+				var url =
+					document.location.protocol + "//" +
+					document.location.hostname + ":" + port +
+					"/?file=" + encodeURIComponent(file.fileName)
+
+				r.open("POST", url, true);
+				r.setRequestHeader("Content-Type", "text/plain") // required for chrome?
+				r.send(file);
+			}
+
+
+			// compute overall progress
+			xfers.soFar = 0
+			for(i = 0; i < xfers.ok.length; i++) {
+				xfers.soFar += xfers.ok[i].size
+			}
+			for(i = 0; i < xfers.error.length; i++) {
+				xfers.soFar += xfers.error[i].size
+			}
+			if(xfers.current) {
+				xfers.soFar += xfers.current.loaded || 0
+			}
+			xfers.percent = Math.floor((xfers.soFar * 100) / xfers.total)
+
+
+			// call back with current status
+			if(cbStatus)
+				cbStatus(xfers)
+
+			setTimeout(self.tick, 250, cbStatus, cbSent)
+		}
+
+	}
+
+/*
+	var dadu = {
+
+		xfers: { files: [] },
+
+		target: function(target, cbStatus, cbEnter, cbLeave, cbSent, url) {
+
+			var xfers = dadu.xfers
+			var nop = function() {}
+
+
+			if(typeof cbStatus === "object") {
+				var o = cbStatus;
+				cbStatus = o.status || nop();
+				cbEnter = o.enter || nop();
+				cbLeave = o.enter || nop();
+				cbSent = o.enter || nop();
+			}
+
+
+			if(typeof target === "string")
+				target = document.getElementById(target)
+			if(typeof target !== "object")
+				target = document.body
+
+			target.ondragenter = function(event) {
+				event.preventDefault();
+				if(cbEnter)
+					cbEnter(event)
+				return true;
+			}
+
+			target.ondragleave = cbLeave || nop
+
+			target.ondragover = function(event) {
+				event.preventDefault();
+				return true;
+			}
+
+			target.ondrop = function(event) {
+				event.preventDefault();
+
+				(cbLeave || nop)(event)
+
+				var newFiles = event.dataTransfer.files
+				var l = newFiles.length
+				var ticking = true
+				var i, file, tick
+
+				if(xfers.files.length < 1 && !xfers.current) {
+					// nothing in queue or in transit; clear counts and arrays
+					ticking = false
+					xfers.ok = []
+					xfers.error = []
+					xfers.current = null
+					xfers.filesTotal = 0
+					xfers.filesDone = 0
+					xfers.filesFaild = 0
+					xfers.total = 0
+					xfers.soFar = 0
+					xfers.percent = 0
+					xfers.done = false
+				}
+
+				// add new files to queue.  there may already be transfers in progress
+				for(i = 0; i < l; i++) {
+					file = newFiles[i]
+					xfers.files.push(file)
+					xfers.total += file.size
+					xfers.filesTotal++
+					if(file.fileName === undefined) {
+						// latest FF
+						file.fileName = file.name;
+					}
+				}
+
+				if(!ticking)
+					dadu.tick(cbStatus, cbSent, url)
+			}
+		},
+
+		tick: function(cbStatus, cbSent, url) {
+			var loc = document.location
+			var xfers = dadu.xfers
+			var l = xfers.files.length
+			var r, file, i , url
+
+			if(!xfers.current) {
+				// nothing currently being sent
+				if(l < 1) {
+					// queue drained. make one last status callback with done=true
+					xfers.done = true;
+					xfers.soFar = xfers.total
+					xfers.percent = 100
+					if(cbStatus)
+						cbStatus(xfers)
+					return	// return, don't restart timer
+				}
+
+				// get next file from queue
+				file = xfers.files.shift()
+
+				// make this the current transfer in progress
+				xfers.current = file
+
+				// start sending it
+				if(typeof ActiveXObject != "undefined") 
+					r = new ActiveXObject("Microsoft.XMLHTTP");
+				else
+					r = new XMLHttpRequest();
+				r.upload.addEventListener("progress", function(e) {
+					if(e.lengthComputable)
+						file.loaded = e.loaded 
+				}, false)
+				r.onload = function() {
+					var hashName = r.responseText
+
+					file.ok = true
+					file.hashName = JSON.parse(r.responseText).hash
+					xfers.ok.push(file)
+					xfers.current = null
+					xfers.filesDone++
+					if(cbSent)
+						cbSent(file)
+				}
+				r.upload.addEventListener("error", function(e) {
+					alert('error');
+					file.error = e
+					xfers.error.push(file)
+					xfers.filesFailed++
+					xfers.current = null
+					xfers.filesDone++
+				}, false)
+				r.upload.addEventListener("abort", function(e) {
+					alert('abort');
+					file.aborted = e
+					xfers.error.push(file)
+					xfers.filesFailed++
+					xfers.current = null
+					xfers.filesDone++
+				}, false)
+				//if(!url) {
+					url = loc.protocol + "//" + loc.hostname + ":4080"
+				//}
+				url += "/?file="+encodeURIComponent(file.fileName)
+				r.open("POST", url, true);
+				r.setRequestHeader("Content-Type", "text/plain") // required for chrome - go figure
+				r.send(file);
+			}
+
+
+			// compute overall progress
+			xfers.soFar = 0
+			for(i = 0; i < xfers.ok.length; i++) {
+				xfers.soFar += xfers.ok[i].size
+			}
+			for(i = 0; i < xfers.error.length; i++) {
+				xfers.soFar += xfers.error[i].size
+			}
+			if(xfers.current) {
+				xfers.soFar += xfers.current.loaded || 0
+			}
+			xfers.percent = Math.floor((xfers.soFar * 100) / xfers.total)
+
+
+			// call back with current status
+			if(cbStatus)
+				cbStatus(xfers)
+
+			setTimeout(dadu.tick, 250, cbStatus, cbSent, url)
+		}
+
+	}
+	*/
+
+
+}
